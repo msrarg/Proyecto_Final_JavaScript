58379bc440493ce0bb4bc58eff8e8f28f3d8e4e8
Mauricio Simón Rodriguez <maurizio_1979_r@hotmail.com>
2022-06-29 18:03:17 -0300

    Va tomando forma

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
 ///
-/// VARIABLES - CLASES - OBJETOS
+/// VARIABLES - CLASES - OBJETOS - IMPORT
 ///
 import { Alumno, Leccion, Intento } from "./src/classes.js";
 
 let students = []; // array donde se irán acumulando los estudiantes.
-let lessons  = []; // array donde se irán acumulando las lecciones.
-let filterLessons = [];
-let attempts = []; // array donde se irán acumulando los intentos de realizar uin lección.
+let allLessons    = []; // array donde se guardan todas las lecciones.
+let filterLessons = []; // array donde se guardan todas las lecciones filtradas de la lección en curso.
+let randomLessons = []; // array donde se guardan todas las lecciones filtradas de la lección en curso deseordenadas.
+let attempts = []; // array donde se irán acumulando los intentos al realizar una lección.
 
-let palabraActual = 0;        // En esta variable guardo la palabra actual de la lección seleccionada.
-let alumnoSeleccionado = 0;   // En esta variable guardo el alumno seleccionado.
-let leccionSeleccionada = ""; // En esta variable guardo la lección seleccionada.
+let currentWord = 0; // En esta variable guardo la palabra actual de la lección seleccionada.
+let currentStudent;  // En esta variable guardo el alumno seleccionado.
+let currentLesson;   // En esta variable guardo la lección seleccionada.
+let currentAttempt;  // En esta variable guardo el número de intento de una lección.
 
-let darkMode = JSON.parse(localStorage.getItem('darkMode')) ?? false;
-// botón darkmode
-const btnDarkMode = document.getElementById('switchDarkMode');
-
-// Función que habilita / deshabilita el darkmode
-const switchDarkMode = (checkDarkMode) => {
-  const body = document.querySelector('body');
-  // console.log(checkDarkMode);
-  if(checkDarkMode){
-      body.classList.add('darkmode');
-  }
-  else {
-      body.classList.remove('darkmode');
-  }
-  btnDarkMode.checked = checkDarkMode;
-  localStorage.setItem('darkMode', JSON.stringify(checkDarkMode));
-}
-
+let darkMode = JSON.parse(localStorage.getItem('darkMode')) ?? false; // Verifico si en el local Storage está almacenada la opción de darkmode.
 //
 // Creo e inicializo las variales para interactuar con el DOM
 //
+const $btnDarkMode       = document.getElementById('switchDarkMode');
 const $formAlumno        = document.getElementById('formAlumno');
 const $btnMostrarAlumnos = document.getElementById('btnMostrarAlumnos');
-const $divAlumnos        = document.getElementById('divAlumnos');
-
-const $pillsAnimalsTab = document.getElementById('pills-animals-tab');
-const $pillsColoursTab = document.getElementById('pills-colours-tab');
-const $pillsMonthsTab  = document.getElementById('pills-months-tab');
+const $divWrapper        = document.getElementById('divWrapper');
+const $pillsAnimalsTab   = document.getElementById('pills-animals-tab');
+const $pillsColoursTab   = document.getElementById('pills-colours-tab');
+const $pillsMonthsTab    = document.getElementById('pills-months-tab');
 
 ///
 ///
@@ -102,42 +87,49 @@ attempts.push(new Intento (1, students[3], "colours", "pink",   1));
 attempts.push(new Intento (1, students[3], "colours", "porple", 0));
 attempts.push(new Intento (1, students[3], "colours", "red",    1));
 attempts.push(new Intento (1, students[3], "colours", "yellow", 1));
-//
-// Modificación del DOM y detección de eventos de usuario.
-//
+
+/// ----------------------------------------------------------------------------------
+/// Modificación del DOM y detección de eventos de usuario.
+/// ----------------------------------------------------------------------------------
 
 // Programo el evento submit en el formAlumno. Ingreso los datos de un alumno nuevo al array students[] mediante el método push.
 $formAlumno.addEventListener('submit', (e) => {
   e.preventDefault();
   let datForm = new FormData(e.target);
-  if(datForm.get('nombres') != "" && datForm.get('apellidos') != "" && datForm.get('email') != ""){
-    let maxIndice = 0;
-    if (students.length > 0) {
-      maxIndice = Math.max(...students.map(alumno => alumno.id));
-    }
-    maxIndice++;
-    let alumno = new Alumno(maxIndice, datForm.get('nombres'), datForm.get('apellidos'), datForm.get('email'), datForm.get('comentario'));
-    students.push(alumno);
-    localStorage.setItem('alumnos', JSON.stringify(students)); // Almacenar datos (clave-valor) en el Storage y recuperarlos.
-    $formAlumno.reset();
-  }
+  let maxIndice = 0;
+  if (students.length > 0) 
+    maxIndice = Math.max(...students.map(alumno => alumno.id));
+  maxIndice++;
+  let alumno = new Alumno(maxIndice, datForm.get('nombres'), datForm.get('apellidos'), datForm.get('email'), datForm.get('comentario'));
+  students.push(alumno);
+  localStorage.setItem('alumnos', JSON.stringify(students)); // Almacenar datos (clave-valor) en el Storage y recuperarlos.
+  $formAlumno.reset();
+  Toastify({
+      text: "El alumno ha sido registrado.",
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
 })
 
 // Programo el evento click en el botón btnMostrarAlumnos del formAlumno. Ingreso los datos de un alumno nuevo al array students[] mediante el método push.
 $btnMostrarAlumnos.addEventListener('click', () => {
-  //let alumnos = JSON.parse(localStorage.getItem('alumnos'))
-  $divAlumnos.innerHTML = ""
+  // let alumnos = JSON.parse(localStorage.getItem('alumnos'))
+  $divWrapper.innerHTML = ""
   if(students.length != 0) {
     students.forEach((alumno) => {
-      $divAlumnos.innerHTML += `
-        <div class="card bg-light mb-3" id="alumno${alumno.id}" style="max-width: 18rem;">
+      $divWrapper.innerHTML += `
+        <div class="card bg-light mb-3" id="alumno${alumno.id}" style="max-width: 22rem;">
         <div class="card-header">Datos del Alumno</div>
           <div class="card-body">
-            <p  class="card-text"> Id:         ${alumno.id}</p>
-            <p  class="card-text"> Nombres:    ${alumno.nombres}</p>
-            <p  class="card-text"> Apellidos:  ${alumno.apellidos}</p>
-            <p  class="card-text"> EMail:      ${alumno.email}</p>
-            <p  class="card-text"> Comentario: ${alumno.comentario}</p>
+            <p class="card-text"> Id:         ${alumno.id}</p>
+            <p class="card-text"> Nombres:    ${alumno.nombres}</p>
+            <p class="card-text"> Apellidos:  ${alumno.apellidos}</p>
+            <p class="card-text"> EMail:      ${alumno.email}</p>
+            <p class="card-text"> Comentario: ${alumno.comentario}</p>
             <button type="button" id="btnSeleccionarAlumno${alumno.id}" class="btn btn-success">Seleccionar Alumno</button>
             <button type="button" id="btnEliminarAlumno${alumno.id}"    class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
           </div>
@@ -146,7 +138,7 @@ $btnMostrarAlumnos.addEventListener('click', () => {
     // Programo el evento click en el botón de Seleccionar Alumno.
     students.forEach((alumno) => {
       document.getElementById(`btnSeleccionarAlumno${alumno.id}`).addEventListener('click', () => {
-        alumnoSeleccionado = alumno.id;
+        currentStudent = alumno;
       })
     })
     // Programo el evento click en el botón de Eliminar Alumno.
@@ -159,78 +151,109 @@ $btnMostrarAlumnos.addEventListener('click', () => {
       })
     })
   } else {
-      $divAlumnos.innerHTML = "<p>No existen alumno cargados para mostrar</p>"
+    // UTILIZACIÓN DE LA LIBRERÍA SWEETALERT 2
+    swal("ALUMNOS REGISTRADOS", "No existen alumnos registrados para mostrar.");
   }
 })
 
 //Evento que se ejecuta para validar si la respuesta es correcta o incorrecta
 $pillsAnimalsTab.addEventListener('click', function (e) {
-  palabraActual = 0;
-  filterLessons = lessons.filter(lesson => lesson.nombre == 'animals');
-  renderleccion(palabraActual);
+  initLesson('animals');
 })
+
 //Evento que se ejecuta para validar si la respuesta es correcta o incorrecta
 $pillsColoursTab.addEventListener('click', function (e) {
-  palabraActual = 0;
-  filterLessons = lessons.filter(lesson => lesson.nombre == 'colours');
-  renderleccion(palabraActual);
+  initLesson('colours');
 })
+
 //Evento que se ejecuta para validar si la respuesta es correcta o incorrecta
 $pillsMonthsTab.addEventListener('click', function (e) {
-  palabraActual = 0;
-  filterLessons = lessons.filter(lesson => lesson.nombre == 'months');
-  renderleccion(palabraActual);
+  initLesson('months');
 })
 
 //Evento que se ejecuta para validar si la respuesta es correcta o incorrecta
 document.getElementById('btnValidar').addEventListener('click', function (e) {
-  validarleccion(palabraActual);
+  validateChosenWord(currentWord);
 })
 
 //Evento que se ejecuta para avanzar de leccion
 document.getElementById('btnSiguiente').addEventListener('click', function (e) {
   // Se incrementa el leccion actual para poder dibujar la tarjeta y para no dejar avanzar mas de los lessons posibles
-  palabraActual++;
-  // const leccion = lessons.filter(less => less.nombre === nombre);
-  if (palabraActual < filterLessons.length){
-    renderleccion(palabraActual);
+  currentWord++;
+  if (currentWord < randomLessons.length){
+    renderWord(currentWord);
   } else {
-    mostarAlerta('exito','Ya no quedan preguntas. Ganaste!');
+    showAlertToastify('exito','Ya no quedan mas preguntas en esta lección.');
   }
 })
 
-///
-/// FUNCIONES
-///
+/// ----------------------------------------------------------------------------------
+/// FUNCIONES - FUNCIONES - FUNCIONES - FUNCIONES - FUNCIONES - FUNCIONES - FUNCIONES  
+/// ----------------------------------------------------------------------------------
 
-// Función que se encarga de validar la respuesta seleccionada por el alumno
-const validarleccion = (numero) => {
-  let palabra = filterLessons[numero];
-  let opcion_elegida = document.querySelector(`input[name="leccionRadio${numero}"]:checked`);
+// Función que inicializa la lección seleccionada y la por defecto al cargar la página.
+const initLesson = (nombre) => {
+  let index = 0;
+  currentWord = 0;
+  currentLesson = nombre;
+  filterLessons = allLessons.filter(less => less.nombre == currentLesson); // Filtro de todas las lecciones la seleccionada
+  // Genero un array aleatorio para que no aparezcan las palabras siempre en el mismo orden.
+  randomLessons = [];
+  do{
+    index = parseInt(Math.random() * (filterLessons.length));
+    if(!randomLessons.includes(filterLessons[index]))
+      randomLessons.push(filterLessons[index]);
+  } while (randomLessons.length < filterLessons.length)
+  renderWord(currentWord);
+  calculateAttempt();
+}
 
-  if (opcion_elegida !== null){
-    if (opcion_elegida.value === palabra.palabraI){
-      mostarAlerta('exito','La opción elegida es la correcta. Puedes avanzar!')
-      document.querySelector('#btnSiguiente').disabled = false;
+// Función que inicializa la lección seleccionada y la por defecto al cargar la página.
+const calculateAttempt = () => {
+  console.log(currentLesson);
+  console.log(currentStudent);
+  let arraytAttempt = attempts.filter((less, stu) => {less.nombre == currentLesson && stu.id == currentStudent.id} ); // Filtro de todas las lecciones la seleccionada
+  console.log(arraytAttempt);
+  console.log(arraytAttempt.length);
+  if (arraytAttempt.length > 0)
+    // currentAttempt = arraytAttempt[0].attempt + 1;
+    currentAttempt = (Math.max(...arraytAttempt) + 1);
+  else
+    currentAttempt = 1;
+  console.log(currentAttempt);
+}
+
+// Función que se encarga de validar la respuesta seleccionada por el alumno en cada palabra de cada lección.
+const validateChosenWord = (numero) => {
+  if(currentStudent){
+    let intento 
+    let palabra = randomLessons[numero];
+    let opcion_elegida = document.querySelector(`input[name="leccionRadio${numero}"]:checked`);
+    if (opcion_elegida !== null){
+      if (opcion_elegida.value === palabra.palabraI){
+        showAlertToastify('exito','La opción elegida es la correcta. Puedes avanzar!')
+        document.querySelector('#btnSiguiente').disabled = false;
+        intento = new Intento(currentAttempt, currentStudent, palabra.nombre, opcion_elegida.value, 1);
+      } else {
+        showAlertToastify('error','La opción elegida no es la correcta');
+        document.querySelector('#btnSiguiente',).disabled = true;
+        intento = new Intento(currentAttempt, currentStudent, palabra.nombre, opcion_elegida.value, 0);
+      }
+      attempts.push(intento);
     } else {
-      mostarAlerta('error','La opción elegida no es la correcta');
-      document.querySelector('#btnSiguiente',).disabled = true;
+        showAlertToastify('alerta','Debe seleccionar una opción para poder validar');
     }
-  } else {
-      mostarAlerta('alerta','Debe seleccionar una opción para poder validar');
-  }   
+  }else{
+    swal("ALUMNOS REGISTRADOS", "No hay un alumno seleccionado para continuar.");
+  }
 }
 
 /// Función que se encarga de dibujar la tarjeta con las opciones posibles.
-const renderleccion = (numero) => { 
-  let palabra = filterLessons[numero];
-  console.log(filterLessons[numero]);
+const renderWord = (numero) => { 
   let html;
-  // let options = filterLessons[numero].getRandomOptions();
-  // <label for="exampleColorInput" class="form-label">Color picker</label>
-  // <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#000000" title="Choose your color"></input>  
-  // padding-top, padding-right, padding-bottom, padding-left).
- 
+  let palabra = randomLessons[numero];
+  let randomOptions = randomLessons[numero].getRandomOptions(); // genero un array aleatorio de las opciones.
+
   if(palabra.nombre == 'colours'){
     let color;
     switch  (palabra.palabraI) {
@@ -274,35 +297,36 @@ const renderleccion = (numero) => {
         color = '#FFFF00';
         break;
     }
+
     html = `
-    <div class="card bg-light mb-3" id="card${palabra.palabraI}" style="max-width: 18rem;">
+    <div class="card bg-light mb-3" id="card${palabra.palabraI}" style="max-width: 22rem;">
       <div class="card-header">${((String)(palabra.nombre)).toUpperCase()}
       </div>
         <div class="row g-0">
           <div class="col-md-4">
             <input type="color" class="form-control form-control-color" id="exampleColorInput" value="${color}"></input>  
-            <img src="./img/${palabra.imagen}" class="img-fluid rounded-start">
+            <img src="../img/${palabra.imagen}" class="img-fluid rounded-start">
           </div>
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">${palabra.palabraE}</h5>`;
   }else{
     html = `
-    <div class="card bg-light mb-3" id="card${palabra.palabraI}" style="max-width: 18rem;">
+    <div class="card bg-light mb-3" id="card${palabra.palabraI}" style="max-width: 22rem;">
       <div class="card-header">${((String)(palabra.nombre)).toUpperCase()}
       </div>
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="./img/${palabra.imagen}" class="img-fluid rounded-start">
+            <img src="../img/${palabra.imagen}" class="img-fluid rounded-start">
           </div>
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">${palabra.palabraE}</h5>`;
   }
-  palabra.opciones.forEach(function (opcion, indice) {
+  randomOptions.forEach(function (option, indice) {
     html += `<div class="form-check">
-              <input class="form-check-input" type="radio" name="leccionRadio${numero}" id="leccionRadio${numero}_${indice}" value="${opcion}">
-              <label class="form-check-label" for="leccionRadio${numero}_${indice}">${opcion}</label>
+              <input class="form-check-input" type="radio" name="leccionRadio${numero}" id="leccionRadio${numero}_${indice}" value="${option}">
+              <label class="form-check-label" for="leccionRadio${numero}_${indice}">${option}</label>
             </div>`
   });
   html += `
@@ -317,7 +341,7 @@ const renderleccion = (numero) => {
 //
 // Esta función se encarga de mostrar el alerta utilizando la librería de Toastify
 //
-const mostarAlerta = (tipo,texto) => {
+const showAlertToastify = (tipo, texto) => {
   let background = '';
   if (tipo === 'exito'){
       background = '#00b09b, #96c93d';
@@ -330,9 +354,9 @@ const mostarAlerta = (tipo,texto) => {
   }
 
   Toastify({
-    text: texto,
-    duration: 3000,
-    gravity: "top",
+    text:     texto,
+    duration: 2000,
+    gravity:  "top",
     position: "right",
     style: {
         background: `linear-gradient(to right, ${background})`
@@ -340,17 +364,33 @@ const mostarAlerta = (tipo,texto) => {
   }).showToast();
 }
 
-/*
-Swal.fire({
-  title: 'Error!',
-  text: 'Do you want to continue?',
-  icon: 'error',
-  confirmButtonText: 'Cool'
-})
-*/
+//Función que cargar las lecciones filtradas por tema (Animals / Colours / Months)
+ const loadLessons = async (path) => {
+  const response = await fetch(path);
+  const jsonLessons = await response.json();
+  jsonLessons.forEach(less => {
+    let leccion = new Leccion(less.nombre, less.palabraE, less.palabraI, less.imagen, less.opciones);
+    allLessons.push(leccion);
+  })
+  initLesson('animals');
+  return response.status === 200 ? true : false;
+}
+
+// Función que permite activar el darkmode
+const switchDarkMode = (checkedDarkMode) => {
+  const body = document.querySelector('body');
+  if(checkedDarkMode)
+      body.classList.add('darkmode');
+  else 
+      body.classList.remove('darkmode');
+  $btnDarkMode.checked = checkedDarkMode;
+  localStorage.setItem('darkMode', JSON.stringify(checkedDarkMode));
+}
 
 // Función inicial
 window.addEventListener("DOMContentLoaded", (event) => {
+
+  // VER SI BORRO -- VER SI BORRO -- VER SI BORRO
   // Implementación con uso de JSON y Storage.
   if(localStorage.getItem('alumnos')){
     // students = JSON.parse(localStorage.getItem('alumnos'));
@@ -358,31 +398,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
     localStorage.setItem('alumnos', JSON.stringify(students))
   }
 
-  // El alert me rompe el código
-  // swal( "Bienvenido a English for Kid!", "Primero deberás crear tu usuario para poder llevar un registro de la evolución de tu aprendizaje. \nLuego selecciona la lección que deseas realizar. ");
-
-  // acción botón darkmode
-  btnDarkMode.onclick = (e) => {
-      const checkDarkMode = btnDarkMode.checked;
-      switchDarkMode(checkDarkMode);
+  // Programo el evento click en el botón btnDarkMode con onclick. Una alternativa al addEventListener.
+  $btnDarkMode.onclick = (e) => {
+    const checkedDarkMode = $btnDarkMode.checked;
+    switchDarkMode(checkedDarkMode);
   };
-
-  //Ejecutamos por primera vez el darkmode
+  // Ejecutamos por primera vez el darkmode
   switchDarkMode(darkMode);
-  loadLessons();
+  loadLessons('..//assets/lessons.json')
+    .then(result => console.log(`${result}`))
+    .catch(e => console.error(`${e}`));
 });
-
-//Función que cargar los productos en el catalogo
-async function loadLessons(){
-  const response = await fetch('./assets/lessons.json');
-  const allLessons = await response.json();
-  allLessons.forEach(lesson => {
-    // let leccion = new Leccion(... less); No funciona
-    let leccion = new Leccion(lesson.nombre, lesson.palabraE, lesson.palabraI, lesson.imagen, lesson.opciones);
-    lessons.push(leccion);
-  })
-
-  palabraActual = 0;  // Palabra inicial.
-  filterLessons = lessons.filter(lesson => lesson.nombre == 'animals'); // Lección por defecto.
-  renderleccion(palabraActual);
-}
--- src/classes.js
+++ src/classes.js
@@ -10,123 +10,8 @@ class Alumno {
         this.email      = email;
         this.comentario = comentario;
     }
-    getNombresApellidos = () => `${this.nombres}, ${this.apellidos}`;
-    // getEdad             = () => Date().getFullYear() - fechaNac.getFullYear(); borrar
 
-    async validarArchivo() {
-        try {
-          // Se trata de leer el archivo para verificar su existencia.
-          let datos = await this.fs.readFile(this.pathFile, this.encoding); 
-            if (!datos) {
-              try{
-                this.fs.writeFile(this.pathFile, '');
-              } 
-              catch(error) {
-                console.log(`Se produjo un error al intentar inicializar el archivo: ${error}`);
-              }
-            }
-            return true;
-        }catch (error) {
-          console.log(`Se produjo un error al intentar leer el archivo: ${error}`);
-          try{
-            // Si no lo puedo leer es porque no existe asi que lo creo y lo inicializo.
-            this.fs.writeFile(this.pathFile, '');
-            console.log(`Se creó exitosamente el archivo`);
-            return true;
-          }catch(e){
-            console.log(`Se produjo un error al intentar crear el archivo: ${e}`);
-            return false;
-          }
-        }
-      }
-    
-      // Se prueba el correcto funcionamiento del método getAll()
-      async getAll() {
-        try {
-          let productos = await this.fs.readFile(this.pathFile, this.encoding); // Se trata de leer el archivo.
-          if (productos){
-            const arrayProductos = JSON.parse(productos); // Read JSON From File
-            return (arrayProductos);
-          }
-          return ([]);
-        } catch (error) {
-          console.log(`Error al leer el archivo en el método getAll(): ${error}`);
-        }
-      }
-    
-      // método getById(Number): Object -> Recibe un id y devuelve el objeto con ese id, o null si no está.
-      async getById(id) {
-        if(!isNaN(id)){
-          try {
-            let arrayProductos = await this.getAll();
-            if (arrayProductos.length > 0){
-              let product = arrayProductos.find((producto) => producto.id === id);
-              return product;
-            }
-            return null;        
-          } catch (error) {
-            console.log(`Error al leer el archivo en el método getById(): ${error}`);
-          }
-        } else {
-          console.log(`El id ${id} no es un número.`);
-        }
-      }
-    
-      // método save(Object): Number -> Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-      async save(producto) {
-        let maxIndice = 0;
-        try {
-          let arrayProductos = await this.getAll();
-          if (arrayProductos.length > 0) {
-            maxIndice = Math.max(...arrayProductos.map(producto => producto.id))
-          }
-          if(!isNaN(maxIndice)){
-            arrayProductos.push({title: producto.title, price: producto.price, thumbnail: producto.thumbnail, id: ++maxIndice}) 
-            this.fs.writeFile(this.pathFile, JSON.stringify(arrayProductos));
-            console.log(`Se agregó el id: ${maxIndice} al archivo`);
-          } else {
-            console.log(`No se pudo calcular el último id en el archivo, id: ${maxIndice}`);
-          }
-          return (maxIndice);     
-        } catch (error) {
-          console.log(`Se produjo un error al intentar guardar el archivo: ${error}`);
-        }
-      }
-        
-      // método deleteById(Number): void -> Elimina del archivo el objeto con el id buscado.
-      async deleteById(id) {
-        try {
-          let object = await this.getById(id);
-          if(object){
-            let arrayProductos = await this.getAll();
-            let newArray = arrayProductos.filter(prod => prod.id != id)
-            this.fs.writeFile(this.pathFile, JSON.stringify(newArray));
-            console.log(`Se ha eliminado el producto con id: ${id}`);
-          } else {
-            console.log(`${id} inexistente.`);
-          }
-        } catch (error) {
-          console.log(`Hubo un error al eliminar un producto por su id: ${error}`);
-        }
-      }
-    
-      // método deleteAll(): void -> Elimina todos los objetos presentes en el archivo.
-      async deleteAll() {
-        try {
-          let arrayProductos = await this.getAll();
-          let cantidad = arrayProductos.length;
-          if (arrayProductos.length > 0) {
-            arrayProductos.length = 0;
-            // this.fs.writeFile(this.pathFile, JSON.stringify(arrayProductos));
-            this.fs.writeFile(this.pathFile, '');
-            console.log(`Se han eliminado: ${cantidad} productos`);
-          } else {
-            console.log(`No existen productos en el archivo para eliminar.`);
-          }
-        } catch (error) {
-          console.log(`Hubo un error al eliminar todos los productos: ${error}`);
-        }
-      }
+    getNombresApellidos = () => `${this.nombres}, ${this.apellidos}`;
   }
   
   class Leccion {
@@ -137,17 +22,18 @@ class Alumno {
         this.imagen   = imagen;
         this.opciones = opciones;
     }
+
     getRandomOptions = () => {
-        let randomOption = [];
-        let index = 0;
-        do{
-          // index = intParse(Math.random() * (this.opciones.length - 1));
-          index = (Math.random() * (this.opciones.length - 1));
+      let index = 0;  
+      let randomOption = [];
+      do{
+          index = parseInt(Math.random() * (this.opciones.length));
             if(!randomOption.includes(this.opciones[index]))
                 randomOption.push(this.opciones[index]);
         } while (randomOption.length < this.opciones.length)
-        return randomOption;
-    };}
+      return randomOption;
+    };
+  }
   
   class Intento {
     constructor(attempt, alumno, lesson, answer, value){
